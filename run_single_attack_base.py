d7c772a81f808376e47c99eb2dce946cbb1bed09
MrCocoaCat <liyubo_liyubo@163.com>
2026-03-25 18:55:29 +0800

    modify singl

--- run_single_attack_base.py
+++ run_single_attack_base.py
@@ -15,6 +15,11 @@ def stream_reader(pipe, label):
 
 def run_single_process(behavior_id: int, device: int, output_path: str,defense:str,behaviors_config:str):
     command = ["python", "attack_llm_core_base_ppl.py", "--id", str(behavior_id), "--device", str(device), "--output_path", output_path,"--defense",defense,"--behaviors_config",behaviors_config]
+    # parser.add_argument('--batch_size', type=int, default=6 )  # 新增：从命令行传参
+    #     parser.add_argument('--top_k', type=int, default=256)  # 新增：从命令行传参
+    #     parser.add_argument('--num_steps', type=int, default=1000)  # 新增：从命令行传参
+    command = ["python", "attack_llm_core_base_select_base_embeding.py", "--id", str(behavior_id), "--device", str(device),
+               "--output_path", output_path, "--defense", defense, "--behaviors_config", behaviors_config, "--batch_size", "512", "--top_k","256", "--num_steps","1000"]
     print(" ".join(command))
     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     
@@ -32,7 +37,67 @@ def run_single_process(behavior_id: int, device: int, output_path: str,defense:s
 
     # Ensure the process completes
     process.communicate()
-    
+
+
+def run_single_process_select_method_cross_entropy(behavior_id: int, device: int, output_path: str, defense: str, behaviors_config: str):
+    command = ["python", "attack_llm_core_base_select_mothed.py", "--id", str(behavior_id),
+               "--device", str(device),
+               "--output_path", output_path,
+              # "--defense", defense,
+               "--behaviors_config", behaviors_config,
+               "--batch_size", "2",
+               "--top_k", "256",
+               "--num_steps", "5",
+               "--loss_type", "cross_entropy"]
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, encoding='utf-8')
+
+    # Create threads to read from stdout and stderr
+    stdout_thread = threading.Thread(target=stream_reader, args=(process.stdout, "STDOUT"))
+    stderr_thread = threading.Thread(target=stream_reader, args=(process.stderr, "STDERR"))
+
+    # Start the threads
+    stdout_thread.start()
+    stderr_thread.start()
+
+    # Wait for threads to finish
+    stdout_thread.join()
+    stderr_thread.join()
+
+    # Ensure the process completes
+    process.communicate()
+
+
+
+def run_single_process_select_method_cosine(behavior_id: int, device: int, output_path: str, defense: str, behaviors_config: str):
+
+    command = ["python", "attack_llm_core_base_select_mothed.py", "--id", str(behavior_id),
+               "--device",str(device),
+               "--output_path", output_path,
+             #  "--defense", defense,
+               "--behaviors_config", behaviors_config,
+               "--batch_size", "2",
+               "--top_k", "256",
+               "--num_steps", "5",
+               "--loss_type","cosine"]
+    print(" ".join(command))
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, encoding='utf-8')
+
+    # Create threads to read from stdout and stderr
+    stdout_thread = threading.Thread(target=stream_reader, args=(process.stdout, "STDOUT"))
+    stderr_thread = threading.Thread(target=stream_reader, args=(process.stderr, "STDERR"))
+
+    # Start the threads
+    stdout_thread.start()
+    stderr_thread.start()
+
+    # Wait for threads to finish
+    stdout_thread.join()
+    stderr_thread.join()
+
+    # Ensure the process completes
+    process.communicate()
+
 if __name__ == "__main__":
     #  --device 0 --output_path Our_GCG_target_len_20/ours/20260226-002518 --defense no_defense --behaviors_config behaviors_config.json
-    run_single_process(BEHAVIOR_ID, DEVICE, OUTPUT_PATH,"no_defense","behaviors_config.json")
+    #run_single_process(BEHAVIOR_ID, DEVICE, OUTPUT_PATH,"no_defense","behaviors_config.json")
+    run_single_process_select_method_cosine(BEHAVIOR_ID, DEVICE, OUTPUT_PATH, "no_defense", "behaviors_config.json")
